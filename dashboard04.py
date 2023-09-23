5f84914b7d2fd6d85f963220fcf50bc97e0aae12
durga-2003 <131599952+durga-2003@users.noreply.github.com>
2023-09-23 18:17:42 +0530

    Update dashboard04.py

--- dashboard04.py
+++ dashboard04.py
@@ -4,7 +4,7 @@ import plotly.express as px
 from PIL import Image
 
 st.set_page_config(page_title='Survey Results')
-st.header('Survey Results 2021')
+st.header('Survey Results 2023')
 st.subheader('Was the tutorial helpful?')
 
 ### --- LOAD DATAFRAME
